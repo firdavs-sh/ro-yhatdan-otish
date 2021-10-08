e35e87d3d06229fd91fc38eb0cc84a0f205db91c
firdavs-sh <92166533+firdavs-sh@users.noreply.github.com>
2021-10-08 18:59:45 +0500

    yukladm

---
+++ script.js
@@ -0,0 +1,7 @@
+function login() {
+var ism=document.getElementById('ism').value;
+var familiya=document.getElementById('familiya').value;
+var email=document.getElementById('email').value;
+var parol=document.getElementById('parol').value;
+document.write(innerHTML=ism+familiya+email+parol)
+}
